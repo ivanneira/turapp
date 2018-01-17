43753a83e9c5b686f954b358a26e9a541c987d23
ivanneira <ien1983@gmail.com>
2018-01-17 16:19:35 -0300

    comienzo del template

--- www/js/noticias.js
+++ www/js/noticias.js
@@ -1,29 +1,29 @@
 var rssurl = 'https://sisanjuan.gob.ar/secciones/ministerio-de-turismo-y-cultura?format=feed';
 
+
 $$(document).on('DOMContentLoaded', function(){
 
+    //esta llamada de ajax tiene que estar dentro de esta funcion onReady para que funcione
     $.ajax({
         url: rssurl,
         success: function(feed) {
-            
-            console.log(feed);
-            console.log("--------------")
+            // console.log(feed);
+            // console.log("--------------")
             getNoticias(feed);
         }
     });
 
 });
 
-
+//objeto xmo
 var contenidos = [];
 
+//trata el xml y deja el html
 function getNoticias(xml){
 
     var items = xml.evaluate("//channel/item/description/text()", xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
 
     var item = items.iterateNext();
-
-    var parser = new DOMParser;
 
     while(item){
         
@@ -33,11 +33,27 @@ function getNoticias(xml){
         item = items.iterateNext();
     }
 
-    console.log(contenidos)
+    showContenidos(contenidos)
 
 }
 
+//transforma el html en el template y lo muestra
+function showContenidos(htmlString){
 
+    var template =  '<div class="card demo-card-header-pic">'+
+                        '<div style="background-image:url(http://lorempixel.com/1000/600/nature/3/)" class="card-header align-items-flex-end">Journey To Mountains</div>'+
+                            '<div class="card-content card-content-padding">'+
+                                '<p class="date">Posted on January 21, 2015</p>'+
+                                '<p>Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non felis. Phasellus quis nibh hendrerit...</p>'+
+                            '</div>'+
+                        '<div class="card-footer"><a href="#" class="link">Like</a><a href="#" class="link">Read more</a></div>'+
+                    '</div>';
+
+    for(var index in htmlString){
+
+        $$("#feeds").append(template);
+    }
+}
 
 
 // var url = rssNoticias;

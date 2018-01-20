19c7c134d5e0a747a37d3be64c9992bedea55220
ivanneira <ien1983@gmail.com>
2018-01-20 18:56:06 -0300

    con senderos.js

--- www/js/noticias.js
+++ www/js/noticias.js
@@ -11,56 +11,16 @@ $$(document).on('DOMContentLoaded', function(){
             getNoticias(feed);
         }
     });*/
-
+    showContenidos(noticiasResult)
 
 });
 
-//objeto xmo
-var contenidos = [];
-
-//console.log(typeof(noticias))
-
-//var a = noticiasResult.category.children;
-
 console.log(noticiasResult)
 
-//getNoticias(noticiasResult);
-
-//trata el json
-function getNoticias(jsonResponse){
-
-
-
-/*    var items = xml.evaluate("//channel/item/description/text()", xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-
-    var item = items.iterateNext();
-
-    while(item){
-
-
-        contenidos.push($(item).text());
-
-        item = items.iterateNext();
-    }*/
-
-    showContenidos(jsonResponse)
-
-}
 
 //transforma el html en el template y lo muestra
 function showContenidos(jsonResponse){
 
-    //console.log(htmlString)
-/*
-    var backgroundImage = "background-image:url(http://lorempixel.com/1000/600/nature/3/)";
-
-    var template =  '<div class="card demo-card-header-pic">'+
-                        '<div style="'+ backgroundImage +'" class="card-header align-items-flex-end">Journey To Mountains</div>'+
-                            '<div class="card-content card-content-padding">'+
-                                '<p class="date">Posted on January 21, 2015</p>'+
-                                '<p>Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non felis. Phasellus quis nibh hendrerit...</p>'+
-                            '</div>'+
-                    '</div>';*/
 
 var header = "<div class=\"card demo-card-header-pic\">";
 var footer = "</div>";
@@ -69,34 +29,8 @@ var template = "<div></div>";
 
     for(var index in jsonResponse){
 
-        $("#feeds").append(header + jsonResponse[index].title + footer);
+        $("#feeds").append(jsonResponse[index].title);
 
         //console.dir($(htmlString[index])[0]);
     }
 }
-
-
-// var url = rssNoticias;
-// var dataType = "xml";
-//
-//
-//     var html = '<ul data-role="listview">';
-//
-//     for(var i = 0; i < feed.items.length; i++) {
-//
-//         var item = feed.items[i];
-//
-//         html += '<li>'
-//             + '<a href="#article?id='
-//             + i
-//             + '">'
-//             + item.title
-//             + '</a>'
-//             + '</li>';
-//     }
-//
-//     html = html + '</ul>';
-//
-//     $$('#feedresult').append(html);
-//     $$('#main').page('destroy').page();
-

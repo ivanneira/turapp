92a4eaf9d27d9694f42f720e1747c1be55e7c59a
Sebassss <pseba20@gmail.com>
2018-01-21 12:09:08 -0300

    leaflet js, mapa quebrada de zonda. intento de inicializar el mapa.

--- www/js/senderos.js
+++ www/js/senderos.js
@@ -1,7 +1,9 @@
 function onPopUpOpen(){
 
-    $$("#senderoContainer").append("<p class='text-color-black'>Este contenido se crea y destruye dinámicamente cabeza</p>")
+    $$("#senderoContainer").append("<p class='text-color-black'>Este contenido se crea y destruye dinámicamente cabeza</p><div id='mapid'></div>")
 
+    var mymap = L.map('mapid').setView([-31.54754175668209, -68.67658624939143], 17);
+    L.tileLayer('mapas/quebradaZonda/{z}/{x}/{y}.jpg',{    maxZoom: 17  }).addTo(mymap);
 
 }
 

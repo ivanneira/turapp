a7065720241a72459d13f2e06a5b1626b4ff46fc
juanmanuelariza <juanma.ariza23@gmail.com>
2018-01-31 01:15:47 -0300

    Plugins, inserts en sqlite, mapa y sendero graficados
    Plugins necesarios (transfer, sqlite, file manager, etc)
    Insert en sqlite de senderos.
    Mapa y sendero graficados.

---
+++ platforms/android/app/src/main/assets/www/js/noticias.js
@@ -0,0 +1,68 @@
+//var rssurl = 'https://sisanjuan.gob.ar/secciones/ministerio-de-turismo-y-cultura?format=json';
+
+
+
+$$(document).on('DOMContentLoaded', function(){
+    //descomentar para hacer la llamada ajax real
+    //esta llamada de ajax tiene que estar dentro de esta funcion onReady para que funcione
+/*    $.ajax({
+        url: rssurl,
+        success: function(feed) {
+            // console.log("--------------")
+            getNoticias(feed);
+        }
+    });*/
+    //showContenidos(noticiasResult)
+
+
+
+});
+
+//console.log(noticiasResult)
+
+console.log("noticiasResult")
+
+//transforma el html en el template y lo muestra
+function showContenidos(jsonResponse){
+
+
+    for ( var i = 0 ; i < jsonResponse.titles.length ; i++){
+
+        var template =  '<div class="card demo-card-header-pic noticia" data-noticiaid="' + [i] + '">' +
+                        '  <div style="background-image:url('+ jsonResponse.image[i]  +')" data-background="'+ jsonResponse.image[i]  +'" class="card-header align-items-flex-end"></div>' +
+                        '  <div class="card-content card-content-padding">' +
+                        '    <p class="date">' + jsonResponse.created[i] + '</p>' +
+                        '    <p>' + jsonResponse.titles[i] + '</p>' +
+                        '  </div>';
+
+        $$("#feeds").append(template);
+    }
+
+    $$(".noticia").click(function(){
+        app.popup.open('.popup-noticias')
+        noticiaID = $(this).data().noticiaid;
+    });
+
+}
+
+function onPopUpNoticiasOpen(){
+
+    //console.log(noticiaID)
+
+    var template =  '<div class="card demo-card-header-pic">' +
+                    '<div style="background-image:url('+ noticiasResult.image[noticiaID] +')" class="card-header align-items-flex-end"></div>' +
+                    '<div class="card-content card-content-padding">' +
+                    '<p class="titulonoticia">' + noticiasResult.titles[noticiaID]+ '</p>' +
+                    '<p class="date">'+ "Creado en " + noticiasResult.created[noticiaID] +'</p>' +
+                    '<div>'+ noticiasResult.fulltext[noticiaID] + '</div>' +
+                    '</div>';
+
+
+    $$("#noticiaContainer").append(template);
+}
+
+function onPopUpNoticiasClose(){
+
+    $$("#noticiaContainer").empty();
+
+}
